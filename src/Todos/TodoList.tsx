2679c44bd7c86f59dcb5fe85326a3fa259f2bfbb
ATmilkyway <natnaelasegd0507@gmail.com>
2025-12-24 02:22:16 +0300

    feat: infinte add more loading

--- src/Todos/TodoList.tsx
+++ src/Todos/TodoList.tsx
@@ -2,40 +2,72 @@ import { useState } from "react";
 import useTodo from "./hooks/useTodo";
 
 const TodoList = () => {
-  const [page, setPage] = useState<number>(1);
+ 
   const pageSize = 10;
-  const { data: todos, error, isLoading } = useTodo(page, pageSize);
+  const { data, error, isLoading ,fetchNextPage} = useTodo(pageSize);
   if (error) return <p>{error.message}</p>;
   if (isLoading) return <p>Loadading ...</p>;
 
   return (
     <>
       <ul>
-        <h1>Page {page}</h1>
-        {todos?.map((todo) => (
-          <li key={todo.id}>
-            {todo.title}
-            <span>{todo.completed ? "Completed" : "Not Completed"}</span>
-          </li>
-        ))}
+        <h1>Page {fetchNextPage.length}</h1>
+          {data?.pages.map(page=>page.map(todo=><li key={todo.id}>{todo.title}</li>))}
       </ul>
       <button
         onClick={() => {
-          setPage(page - 1);
+         fetchNextPage()
         }}
-        disabled={page < 2}
+        // disabled={fetchNextPage.length < 1}
       >
-        Previous
+        Load More
       </button>
-      <button
-        onClick={() => {
-          setPage(page + 1);
-        }}
-      >
-        Next
-      </button>
+      
     </>
   );
 };
 
 export default TodoList;
+
+
+// import { useState } from "react";
+// import useTodo from "./hooks/useTodo";
+
+// const TodoList = () => {
+//   const [page, setPage] = useState<number>(1);
+//   const pageSize = 10;
+//   const { data: todos, error, isLoading } = useTodo(page, pageSize);
+//   if (error) return <p>{error.message}</p>;
+//   if (isLoading) return <p>Loadading ...</p>;
+
+//   return (
+//     <>
+//       <ul>
+//         <h1>Page {page}</h1>
+//         {todos?.map((todo) => (
+//           <li key={todo.id}>
+//             {todo.title}
+//             <span>{todo.completed ? "Completed" : "Not Completed"}</span>
+//           </li>
+//         ))}
+//       </ul>
+//       <button
+//         onClick={() => {
+//           setPage(page - 1);
+//         }}
+//         disabled={page < 2}
+//       >
+//         Previous
+//       </button>
+//       <button
+//         onClick={() => {
+//           setPage(page + 1);
+//         }}
+//       >
+//         Next
+//       </button>
+//     </>
+//   );
+// };
+
+// export default TodoList;
--- src/Todos/hooks/useTodo.ts
+++ src/Todos/hooks/useTodo.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+ import { useState } from "react";
 import apiClient from "../service/apiClient";
 
 interface Todo {
@@ -8,20 +8,57 @@ interface Todo {
   completed: boolean;
 }
 
-const useTodo = (page: number, pageSize: number) => {
-  return useQuery<Todo[]>({
-    queryKey: ["todos", page],
-    queryFn: () =>
+// const useTodo = (page: number, pageSize: number) => {
+//   return useQuery<Todo[]>({
+//     queryKey: ["todos", page],
+//     queryFn: () =>
+//       apiClient
+//         .get("/todos", {
+//           params: {
+//             _start: (page - 1) * pageSize,
+//             _limit: pageSize,
+//           },
+//         })
+//         .then((res) => res.data),
+//     staleTime: 1000 * 60 * 1,
+//     gcTime: 1000 * 60 * 1,
+//   });
+// };
+
+// export default useTodo;
+
+import { keepPreviousData, useInfiniteQuery, type InfiniteData } from "@tanstack/react-query";
+ 
+interface Todo {
+  userId: string;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+ 
+const useTodo = (pageSize:number) => {
+  const [pageNumber,setPageNumber]=useState(1)
+  return useInfiniteQuery<Todo[], Error, InfiniteData<Todo[], number>, ["todos"], number>({
+    queryKey: ["todos"],
+    initialPageParam: 1, 
+
+    queryFn: ({ pageParam }) =>
       apiClient
-        .get("/todos", {
-          params: {
-            _start: (page - 1) * pageSize,
-            _limit: pageSize,
-          },
+        .get<Todo[]>("/todos", {
+          params: { 
+            _start:(pageParam-1)*pageSize,
+            _limit:pageSize
+           },
         })
-        .then((res) => res.data),
-    staleTime: 1000 * 60 * 1,
-    gcTime: 1000 * 60 * 1,
+        .then(res => res.data),
+
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.length === 0
+        ? undefined
+        : allPages.length + 1
+    },
+  placeholderData: keepPreviousData, 
+
   });
 };
 
